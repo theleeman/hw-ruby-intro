a3ec7011e58206ac3147ed842b3b2ef4482f8419
khim chea <kchea91@gmail.com>
2018-04-18 14:18:11 +0000

    part1 completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,21 +1,53 @@
 # When done, submit this entire file to the autograder.
 
 # Part 1
-
-def sum arr
-  # YOUR CODE HERE
+#Define a method sum(arr) that takes an array of integers 
+#as an argument and returns the sum of its elements. 
+#For an empty array it should return zer. 
+def sum(arr)
+  sum = 0
+  
+  sum if arr.length == 0
+  arr.each do |x|
+      sum += x
+  end
+  sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+#Define a method max_2_sum(arr) which takes an array of integers
+#as an argument and returns the sum of its two largest elements. 
+#For an empty array it should return zero. For an array with just one
+#element, it should return that element. 
+def max_2_sum(arr)
+  arr = arr.sort
+  sum = 0
+  
+  if arr.length == 0
+     sum
+  elsif arr.length == 1
+     arr[0]
+  else
+    sum =  arr[(arr.length - 1)] + arr[(arr.length - 2)] 
+  end
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+
+#Define a method sum_to_n(array,n) that takes an array of integers and an additional 
+#integer, n, as arguments and returns true if any two elements in the array of integers sum to n
+def sum_to_n?(arr, n)
+  
+  if arr.length < 2
+    false
+  else
+    arr.permutation(2).any? {|x, y| x + y == n}
+    
+  end
 end
+
+
 
 # Part 2
-
+=begin
 def hello(name)
   # YOUR CODE HERE
 end
@@ -33,3 +65,4 @@ end
 class BookInStock
 # YOUR CODE HERE
 end
+=end
